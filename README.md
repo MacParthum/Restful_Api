a60771386c109d9044e72526c9591e2a95627546
Mac Parthum <macparthum@gmail.com>
2020-11-12 16:13:39 -0500

    connected to mongodb atlas

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ A restful Api with Node.js Express and MongoDB
 
 ## Flow
 1. npm init to create a packgage json file
-2. npm install express nodemon
+2. npm install express nodemon mongoose env
 4. change script in package json on line 7 (executes and runs the server)
+5. use "connect to application" on mongo.db atlas https://docs.atlas.mongodb.com/security/add-ip-address-to-list/
--- app.js
+++ app.js
@@ -1,11 +1,31 @@
+// import the package
 const express = require('express');
 
+// execute the package
 const app = express ();
+const mongoose = require ('mongoose');
 
-// Routes
+// middleware - a function that executes when a route is hit
+// app.use ('/posts', () => {
+//     console.log('This is a middleware running');
+//  });
+
+
+
+// route to home
 app.get ('/', (req, res) => {
    res.send ('We are on home');
 });
+// route to posts
+app.get ('/posts', (req, res) => {
+    res.send ('We are on posts');
+ });
+
+ // connect to db using mongo db atlas 
+ mongoose.connect("mongodb+srv://Mac:Harlan1370@restful.0us68.mongodb.net/restful?retryWrites=true&w=majority" , () =>
+  console.log ('Connected to DB!')
+ );
+
 
 // Start listening on port 3000
 app.listen (3000);
