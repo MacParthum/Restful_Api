046418cecc2c3c929e953c4d2610ba12fe921a96
Mac Parthum <macparthum@gmail.com>
2020-11-12 16:52:59 -0500

    created routes folder and posts.js to clean up the app.js file

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
-// import the package
 const express = require('express');
-
-// execute the package
 const app = express ();
-
 const mongoose = require ('mongoose');
-
 require('dotenv/config');
 
+const postsRoute = require ('./routes/posts');
+
+app.use ('/posts', postsRoute);
 
 // route to home
 app.get ('/', (req, res) => {
    res.send ('We are on home');
 });
-// route to posts
-app.get ('/posts', (req, res) => {
-    res.send ('We are on posts');
- });
 
  // connect to db using mongo db atlas 
- // mongodb+srv://Mac:<password>@restful.0us68.mongodb.net/restful?retryWrites=true&w=majority
- mongoose.connect(process.env.DB_CONNECTION,
+ mongoose.connect(
+    process.env.DB_CONNECTION,
     { useNewUrlParser: true},
     () => console.log ('Connected to DB!')
  );
-
 
 // Start listening on port 3000
 app.listen (3000);
